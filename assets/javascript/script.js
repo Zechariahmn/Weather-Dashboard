3ea64f3e98795af3c43e0678f121cd24dfe571f0
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-04 16:03:11 -0500

    declard the variables needed for the application

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+//Declare a variable to store the searched city
+let city="";
+
+// variable declaration
+let searchCity = $("#search-city");
+let searchButton = $("#search-button");
+let clearButton = $("#clear-history");
+let currentCity = $("#current-city");
+let currentTemperature = $("#temperature");
+let currentHumidty= $("#humidity");
+let currentWSpeed=$("#wind-speed");
+let currentUvindex= $("#uv-index");
+let sCity=[];
